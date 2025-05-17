af2da8262624fd5da422c5db0763949594e13bea
maneeshsagar <consultant2@powerxchange.io>
2025-05-17 14:24:20 +0530

    added the eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js
+
+module.exports = {
+    root: true,
+    extends: ["next/core-web-vitals"],
+    rules: {
+      // Customize your lint rules here
+      // For example:
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+    },
+  };
+  
